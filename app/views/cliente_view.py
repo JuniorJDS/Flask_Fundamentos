51208e515a77e9a5d5cc2f320fd85548f1ddb566
JuniorJDS <juniorjds2015@gmail.com>
2020-08-21 23:19:10 -0300

    Atualizando o método de remoção

--- app/views/cliente_view.py
+++ app/views/cliente_view.py
@@ -1,4 +1,4 @@
-from flask import render_template, redirect, url_for
+from flask import render_template, redirect, url_for, request
 from app import app
 from app.forms import cliente_form
 from app import db
@@ -42,4 +42,44 @@ def listar_clientes():
 @app.route("/listar_cliente/<int:id>")
 def listar_cliente(id):
     cliente = cliente_model.Cliente.query.filter_by(id=id).first()
-    return cliente.nome
+    return render_template("clientes/lista_cliente.html", cliente=cliente)
+
+
+@app.route("/editar_cliente/<int:id>", methods=["POST", "GET"])
+def editar_cliente(id):
+    cliente = cliente_model.Cliente.query.filter_by(id=id).first()
+    form = cliente_form.ClientForm(obj=cliente)
+    if form.validate_on_submit():
+        nome = form.nome.data
+        email = form.email.data
+        data_nascimento = form.data_nascimento.data
+        profissao = form.profissao.data
+        sexo = form.sexo.data
+
+        cliente.nome = nome
+        cliente.email = email
+        cliente.data_nascimento = data_nascimento
+        cliente.profissao = profissao
+        cliente.sexo = sexo
+        try:
+            db.session.commit()
+            return redirect(url_for("listar_clientes"))
+        except:
+            print("Cliente não foi Editado!")
+
+    return render_template("clientes/form.html", form=form)
+
+
+@app.route("/remover_cliente/<int:id>", methods=["GET", "POST"])
+def remover_cliente(id):
+    cliente = cliente_model.Cliente.query.filter_by(id=id).first()
+
+    if request.method == "POST":
+        try:
+            db.session.delete(cliente)
+            db.session.commit()
+            return redirect(url_for("listar_clientes"))
+        except:
+            print("Cliente não foi Removido!")
+
+    return render_template("clientes/remover_cliente.html", cliente=cliente)
