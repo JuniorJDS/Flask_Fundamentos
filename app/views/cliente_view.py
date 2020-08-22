35fbb297f0e8225cebd4b313a475fc83875e0dbf
JuniorJDS <juniorjds2015@gmail.com>
2020-08-22 00:19:13 -0300

    refatorando...

--- app/views/cliente_view.py
+++ app/views/cliente_view.py
@@ -1,8 +1,9 @@
 from flask import render_template, redirect, url_for, request
 from app import app
 from app.forms import cliente_form
-from app import db
 from app.models import cliente_model
+from app.entidades import cliente
+from app.services import cliente_service
 
 
 @app.route("/cadastrar_cliente", methods=['GET', 'POST'])
@@ -18,8 +19,7 @@ def cadastrar_cliente():
         cliente = cliente_model.Cliente(nome=nome, email=email, data_nascimento=data_nascimento,
                                         profissao=profissao, sexo=sexo)
         try:
-            db.session.add(cliente)
-            db.session.commit()
+            cliente_service.cadastrar_cliente(cliente)
             return redirect(url_for("listar_clientes"))
         except:
             print("Cliente não cadastrado!")
@@ -36,19 +36,19 @@ def login():
 
 @app.route("/listar_clientes", methods=["GET"])
 def listar_clientes():
-    clientes = cliente_model.Cliente.query.all()
+    clientes = cliente_service.listar_clientes()
     return render_template("clientes/lista_clientes.html", clientes=clientes)
 
 @app.route("/listar_cliente/<int:id>")
 def listar_cliente(id):
-    cliente = cliente_model.Cliente.query.filter_by(id=id).first()
+    cliente = cliente_service.listar_cliente(id)
     return render_template("clientes/lista_cliente.html", cliente=cliente)
 
 
 @app.route("/editar_cliente/<int:id>", methods=["POST", "GET"])
 def editar_cliente(id):
-    cliente = cliente_model.Cliente.query.filter_by(id=id).first()
-    form = cliente_form.ClientForm(obj=cliente)
+    cliente_bd = cliente_service.listar_cliente(id)
+    form = cliente_form.ClientForm(obj=cliente_bd)
     if form.validate_on_submit():
         nome = form.nome.data
         email = form.email.data
@@ -56,13 +56,11 @@ def editar_cliente(id):
         profissao = form.profissao.data
         sexo = form.sexo.data
 
-        cliente.nome = nome
-        cliente.email = email
-        cliente.data_nascimento = data_nascimento
-        cliente.profissao = profissao
-        cliente.sexo = sexo
+        cliente_novo = cliente.Cliente(nome=nome, email=email, data_nascimento=data_nascimento,
+                                        profissao=profissao, sexo=sexo)
+
         try:
-            db.session.commit()
+            cliente_service.editar_cliente(cliente_bd, cliente_novo)
             return redirect(url_for("listar_clientes"))
         except:
             print("Cliente não foi Editado!")
@@ -72,12 +70,11 @@ def editar_cliente(id):
 
 @app.route("/remover_cliente/<int:id>", methods=["GET", "POST"])
 def remover_cliente(id):
-    cliente = cliente_model.Cliente.query.filter_by(id=id).first()
+    cliente = cliente_service.listar_cliente(id)
 
     if request.method == "POST":
         try:
-            db.session.delete(cliente)
-            db.session.commit()
+            cliente_service.remover_cliente(cliente)
             return redirect(url_for("listar_clientes"))
         except:
             print("Cliente não foi Removido!")
