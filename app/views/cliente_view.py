7788d232e8d1083e66c704c5f1c47544134523b1
JuniorJDS <juniorjds2015@gmail.com>
2020-08-24 22:36:15 -0300

    adc requirements.txt

--- app/views/cliente_view.py
+++ app/views/cliente_view.py
@@ -16,10 +16,10 @@ def cadastrar_cliente():
         profissao = form.profissao.data
         sexo = form.sexo.data
 
-        cliente = cliente_model.Cliente(nome=nome, email=email, data_nascimento=data_nascimento,
-                                        profissao=profissao, sexo=sexo)
+        cli = cliente_model.Cliente(nome=nome, email=email, data_nascimento=data_nascimento,
+                                    profissao=profissao, sexo=sexo)
         try:
-            cliente_service.cadastrar_cliente(cliente)
+            cliente_service.cadastrar_cliente(cli)
             return redirect(url_for("listar_clientes"))
         except:
             print("Cliente não cadastrado!")
@@ -39,16 +39,18 @@ def listar_clientes():
     clientes = cliente_service.listar_clientes()
     return render_template("clientes/lista_clientes.html", clientes=clientes)
 
+
 @app.route("/listar_cliente/<int:id>")
 def listar_cliente(id):
-    cliente = cliente_service.listar_cliente(id)
-    return render_template("clientes/lista_cliente.html", cliente=cliente)
+    cliente_list = cliente_service.listar_cliente(id)
+    return render_template("clientes/lista_cliente.html", cliente=cliente_list)
 
 
 @app.route("/editar_cliente/<int:id>", methods=["POST", "GET"])
 def editar_cliente(id):
     cliente_bd = cliente_service.listar_cliente(id)
     form = cliente_form.ClientForm(obj=cliente_bd)
+    #form.sexo.data = cliente_bd.sexo
     if form.validate_on_submit():
         nome = form.nome.data
         email = form.email.data
@@ -57,7 +59,7 @@ def editar_cliente(id):
         sexo = form.sexo.data
 
         cliente_novo = cliente.Cliente(nome=nome, email=email, data_nascimento=data_nascimento,
-                                        profissao=profissao, sexo=sexo)
+                                       profissao=profissao, sexo=sexo)
 
         try:
             cliente_service.editar_cliente(cliente_bd, cliente_novo)
@@ -70,13 +72,13 @@ def editar_cliente(id):
 
 @app.route("/remover_cliente/<int:id>", methods=["GET", "POST"])
 def remover_cliente(id):
-    cliente = cliente_service.listar_cliente(id)
+    cliente_list = cliente_service.listar_cliente(id)
 
     if request.method == "POST":
         try:
-            cliente_service.remover_cliente(cliente)
+            cliente_service.remover_cliente(cliente_list)
             return redirect(url_for("listar_clientes"))
         except:
             print("Cliente não foi Removido!")
 
-    return render_template("clientes/remover_cliente.html", cliente=cliente)
+    return render_template("clientes/remover_cliente.html", cliente=cliente_list)
